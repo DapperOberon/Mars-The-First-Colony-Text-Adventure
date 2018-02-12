e89aa279bbfd7e720decba180bee75dd5f3ab008
Cameron Smith <camerosmit@yahoo.com>
2018-02-12 14:49:43 -0800

    Created the initial game states

---
+++ Mars - The First Colony Text Adventure/Assets/GameAssets/Scripts/GUIManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GUIManager : MonoBehaviour {
+
+	public static GUIManager instance = null;
+
+	public GameObject _mainMenu;
+	public GameObject _chapterOne;
+
+	public TMP_Text _storyText;
+
+	private void Awake()
+	{
+		if(instance == null)
+		{
+			instance = this;
+		}
+		else if (instance != this)
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	private void Start()
+	{
+		// Reset scenes to default start
+		_mainMenu.SetActive(true);
+		_chapterOne.SetActive(false);
+	}
+
+	private void Update()
+	{
+		if (Input.anyKey)
+		{
+			_mainMenu.SetActive(false);
+			_chapterOne.SetActive(true);
+		}
+	}
+}
---
+++ Mars - The First Colony Text Adventure/Assets/GameAssets/Scripts/Game.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+
+	// Setup chapters and scenes
+	public Chapter chapter = Chapter.One;
+	public enum Chapter { One, Two, Three };
+	public Scene scene = Scene.Start;
+	public enum Scene { Start };
+
+	private void Update()
+	{
+		switch (chapter)
+		{
+			case Chapter.One:
+				ChapterOne();
+				break;
+			case Chapter.Two:
+				ChapterTwo();
+				break;
+			case Chapter.Three:
+				ChapterThree();
+				break;
+		}
+	}
+
+	// Chapters
+	#region
+	// One
+	private void ChapterOne()
+	{
+		switch (scene)
+		{
+			case Scene.Start:
+				SceneStart();
+				break;
+		}
+	}
+
+	// Two
+	private void ChapterTwo()
+	{
+
+	}
+
+	// Three
+	private void ChapterThree()
+	{
+
+	}
+	#endregion
+
+	// Scenes
+	#region
+	// Start
+	private void SceneStart()
+	{
+		GUIManager.instance._storyText.text = "This is the story text. Write your scene here...";
+	}
+	#endregion
+}
