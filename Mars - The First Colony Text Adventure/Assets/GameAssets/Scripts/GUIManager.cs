137dc1aa2da42f086d4f7ea12f02c39c0a1fd144
Cameron Smith <camerosmit@yahoo.com>
2018-08-02 14:41:25 -0700

    Added two screen fades: a default first screen fade and a custom from - to fade.

--- Mars - The First Colony Text Adventure/Assets/GameAssets/Scripts/GUIManager.cs
+++ Mars - The First Colony Text Adventure/Assets/GameAssets/Scripts/GUIManager.cs
@@ -1,17 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
+using UnityEngine.UI;
 
 public class GUIManager : MonoBehaviour {
 
 	public static GUIManager instance = null;
 
 	public GameObject _mainMenu;
-	public GameObject _chapterOne;
-
+	public GameObject _story;
+	public GameObject _storyBg;
 	public TextMeshProUGUI _storyText;
+
+	[Space]
+	public GameObject _chapterOne;
+	public GameObject _chapterTwo;
+
+	private Animator anim;
 
 	private void Awake()
 	{
@@ -27,17 +33,43 @@ public class GUIManager : MonoBehaviour {
 
 	private void Start()
 	{
+		anim = GetComponent<Animator>();
+
 		// Reset scenes to default start
 		_mainMenu.SetActive(true);
+		_story.SetActive(false);
+		_storyBg.SetActive(false);
 		_chapterOne.SetActive(false);
+		_chapterTwo.SetActive(false);
 	}
 
 	private void Update()
 	{
 		if (Input.anyKey)
 		{
-			_mainMenu.SetActive(false);
-			_chapterOne.SetActive(true);
+			StartCoroutine(FirstFade());
 		}
+	}
+
+	private IEnumerator FirstFade()
+	{
+		anim.Play("GUIFade");
+
+		yield return new WaitForSeconds(1.5f); // Half of the total length of fade clip. If clip length is change, this value must change too.
+
+		_mainMenu.SetActive(false);
+		_story.SetActive(true);
+		_storyBg.SetActive(true);
+		_chapterOne.SetActive(true);
+	}
+
+	private IEnumerator Fade(GameObject from, GameObject to)
+	{
+		anim.Play("GUIFade");
+
+		yield return new WaitForSeconds(1.5f); // Half of the total length of fade clip. If clip length is change, this value must change too.
+
+		from.SetActive(false);
+		to.SetActive(true);
 	}
 }
